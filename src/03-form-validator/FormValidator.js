8f43d0ce543f8b500b56804996bf9d234e02fbcd
Lilia Brown <liliabrown24@gmail.com>
2024-03-28 13:10:49 -0600

    added validation to form

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -1,28 +1,62 @@
 import { useState } from 'react'
 
 export default function FormValidator () {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const errors = {};
+
+    if (!email) {
+      errors.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    if (!password) {
+      errors.password = 'Password is required';
+    } else if (password.length < 8) {
+      errors.password = 'Password must be at least 8 characters long';
+    }
+
+    if (!passwordConfirm) {
+      errors.passwordConfirm = 'Please re-enter your password';
+    } else if (password !== passwordConfirm) {
+      errors.passwordConfirm = 'Passwords must match';
+    }
+
+    if (Object.keys(errors).length > 0) {
+      setErrors(errors);
+    } else {
+      console.log('Form submitted');
+    }
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
         type='text' name='email'
         onChange={e => setEmail(e.target.value)}
       />
+      <div>{errors.email}</div>
       <label htmlFor='password'>Password</label>
       <input
         type='password' name='password'
         onChange={e => setPassword(e.target.value)}
       />
+      <div>{errors.password}</div>
       <label htmlFor='password-confirm'>Confirm Password </label>
       <input
         type='password' name='password-confirm'
         onChange={e => setPasswordConfirm(e.target.value)}
       />
+      <div>{errors.passwordConfirm}</div>
       <input type='submit' value='Submit' />
     </form>
   )
