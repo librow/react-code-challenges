7a16b1dafb4820ec617d79edce796df5e376734e
Lilia Brown <liliabrown24@gmail.com>
2024-04-01 13:12:10 -0600

    loading dog pics

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,25 @@
+import { useState, useEffect } from 'react'
+
+const getDogPic = async () => {
+  const api = 'https://dog.ceo/api/breeds/image/random';
+  const response = await fetch(api);
+  const dog = await response.json();
+  
+  return dog.message;
+}
+
 export default function DogPics () {
   // API: https://dog.ceo/dog-api/
+  const [dogPic, setDogPic] = useState('');
+
+  useEffect(() => {
+    getDogPic().then(pic => setDogPic(pic));
+  }, [])
+
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <img src={dogPic} alt='dog pic' />
+      <button onClick={async e => setDogPic(await getDogPic())}>🐶</button>
     </div>
   )
 }
