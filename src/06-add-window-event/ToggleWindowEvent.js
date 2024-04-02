405828cc099e9766089215c8c81774dba57992a3
Lilia Brown <liliabrown24@gmail.com>
2024-04-02 10:29:28 -0600

    added event listener

--- src/06-add-window-event/ToggleWindowEvent.js
+++ src/06-add-window-event/ToggleWindowEvent.js
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import WindowEvent from './WindowEvent'
 
 export default function ToggleWindowEvent () {
-  const [windowEvent, setWindowEvent] = useState(false)
+  const [windowEvent, setWindowEvent] = useState(false);
+
   return (
     <div>
       <button onClick={() => setWindowEvent(prevState => !prevState)}>Toggle Window Event</button>
--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,4 +1,14 @@
+import { useEffect } from 'react'
+
 export default function WindowEvent () {
+  useEffect(() => {
+    const handleDoubleClick = () => alert('worked');
+    
+    document.addEventListener("dblclick", handleDoubleClick);
+    
+    return () => document.removeEventListener("dblclick", handleDoubleClick);
+  }, [])
+
   return (
     <h2>Window event active</h2>
   )
